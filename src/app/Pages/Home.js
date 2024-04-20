3b06d26e87831649fd3d7d43f0952539b8a4c86d
Kuntal <kuntalpanchal97@gmail.com>
2024-04-20 14:10:27 +0530

    fifth commit - imporving pagination ui and adding more dummy data

--- src/app/Pages/Home.js
+++ src/app/Pages/Home.js
@@ -1,14 +1,11 @@
 "use client";
 import { MdOutlineFilterAlt } from "react-icons/md";
 import { IoIosCloseCircleOutline } from "react-icons/io";
-import { PiNumberSquareOneLight } from "react-icons/pi";
 import { GrPrevious, GrNext } from "react-icons/gr";
 import RowData from "../RowData/RowData";
 import { useState } from "react";
 
-// Define the Home component
 export default function Home() {
-	// Use useState hook to manage state
 	const [currentPage, setCurrentPage] = useState(0);
 	const [rowsPerPage, setRowsPerPage] = useState(3);
 
@@ -18,7 +15,11 @@ export default function Home() {
 			time: "14:05",
 			reportName: "APP_URL_USAGE_(Exclude_Half_Days)_3_6_2021.csv",
 		},
-		{ date: "23.07.2021", time: "15:20", reportName: "REPORT_2021_Q2.csv" },
+		{
+			date: "23.07.2021",
+			time: "15:20",
+			reportName: "REPORT_2021_Q2.csv",
+		},
 		{
 			date: "24.07.2021",
 			time: "11:45",
@@ -33,6 +34,91 @@ export default function Home() {
 			date: "26.07.2021",
 			time: "09:15",
 			reportName: "EMPLOYEE_TIMESHEETS_JULY_2021.csv",
+		},
+		{
+			date: "27.07.2021",
+			time: "08:00",
+			reportName: "REPORT_2021_Q3.csv",
+		},
+		{
+			date: "28.07.2021",
+			time: "09:30",
+			reportName: "SALES_REPORT_AUGUST_2021.csv",
+		},
+		{
+			date: "29.07.2021",
+			time: "10:45",
+			reportName: "ANNUAL_FINANCIAL_REPORT.csv",
+		},
+		{
+			date: "30.07.2021",
+			time: "11:20",
+			reportName: "EMPLOYEE_TIMESHEETS_AUGUST_2021.csv",
+		},
+		{
+			date: "31.07.2021",
+			time: "12:15",
+			reportName: "MONTHLY_SALES_SUMMARY.csv",
+		},
+		{
+			date: "01.08.2021",
+			time: "13:45",
+			reportName: "QUARTERLY_REPORT_Q3.csv",
+		},
+		{
+			date: "02.08.2021",
+			time: "14:30",
+			reportName: "ANNUAL_BUDGET_OVERVIEW.csv",
+		},
+		{
+			date: "03.08.2021",
+			time: "15:10",
+			reportName: "EMPLOYEE_PERFORMANCE_REVIEW.csv",
+		},
+		{
+			date: "04.08.2021",
+			time: "16:00",
+			reportName: "MONTHLY_EXPENSES.csv",
+		},
+		{
+			date: "05.08.2021",
+			time: "17:20",
+			reportName: "PRODUCT_INVENTORY.csv",
+		},
+		{
+			date: "06.08.2021",
+			time: "18:15",
+			reportName: "QUARTERLY_FINANCIAL_ANALYSIS_Q3.csv",
+		},
+		{
+			date: "07.08.2021",
+			time: "19:30",
+			reportName: "CUSTOMER_FEEDBACK_SURVEY.csv",
+		},
+		{
+			date: "08.08.2021",
+			time: "20:40",
+			reportName: "ANNUAL_REVENUE_REPORT.csv",
+		},
+		{
+			date: "09.08.2021",
+			time: "21:25",
+			reportName: "EMPLOYEE_SALARY_STATEMENT.csv",
+		},
+		{
+			date: "10.08.2021",
+			time: "22:10",
+			reportName: "MONTHLY_MARKETING_ANALYTICS.csv",
+		},
+		{
+			date: "11.08.2021",
+			time: "23:05",
+			reportName: "PRODUCT_SALES_REPORT.csv",
+		},
+		{
+			date: "12.08.2021",
+			time: "08:30",
+			reportName: "QUARTERLY_PROFIT_ANALYSIS_Q3.csv",
 		},
 	];
 
@@ -100,15 +186,19 @@ export default function Home() {
 							</button>
 							<div className="flex">
 								{Array.from({ length: totalPages }).map((_, index) => (
-									<PiNumberSquareOneLight
+									<button
+										className="border rounded-sm px-2 mx-1 "
 										key={index}
 										size={30}
 										style={{
 											cursor: "pointer",
-											opacity: index === currentPage ? 1 : 0.6,
+											backgroundColor:
+												index === currentPage ? "orange" : "white",
+											color: index === currentPage ? "white" : "black",
 										}}
-										onClick={() => setCurrentPage(index)}
-									/>
+										onClick={() => setCurrentPage(index)}>
+										{index}
+									</button>
 								))}
 							</div>
 							<button
@@ -125,9 +215,13 @@ export default function Home() {
 								className="rounded-sm text-sm border"
 								value={rowsPerPage}
 								onChange={handleChangeRowsPerPage}>
+								<option value="1">1</option>
+								<option value="2">2</option>
 								<option value="3">3</option>
+								<option value="4">4</option>
 								<option value="5">5</option>
-								<option value="10">10</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
 							</select>
 						</div>
 					</div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
-// page.js
-
-// Import necessary modules and components
 import Home from "./Pages/Home";
 import RootLayout from "./layout";
 
-// Export default function to render the Next.js application
 export default function App() {
 	return (
 		<RootLayout>
